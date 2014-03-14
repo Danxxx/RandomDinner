4db1af7edf27e0468a3fc75c3d3caa3f9cf721bf
DanX <dan@dan-solutions.dk>
2014-03-14 13:39:53 +0100

    foodlib add

---
+++ routes/foodlib.js
@@ -0,0 +1,27 @@
+/**
+ * Created by danx on 14-03-14.
+ */
+
+module.exports.test = function(){
+    console.log('test');
+    var randomFood = food[RandomNummber()];
+    return (randomFood);
+}
+
+var food = [
+    {
+        name: 'Bøf med løg'
+    },
+    {
+        name: 'Kylling med Pomfritter'
+    },
+    {
+        name: 'Flæskestag med katofler'
+    }
+];
+
+
+var RandomNummber = function(){
+    var num = Math.floor(Math.random() * 3) + 1;
+    return num;
+};
--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    food = require('./routes/foods');
+    url = require('url'),
+    foodlib = require('./routes/foodlib');
 
-var app = express();
-
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.get('/food', food.GetRandomFood);
-app.get('/food/:id', food.GetRandomFood);
-
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+http.createServer(function(req, res) {
 
     var parsedUrl = url.parse(req.url, true); // true to get query as object
     var queryAsObject = parsedUrl.query;
 
     console.log(JSON.stringify(queryAsObject));
+    var test = foodlib.test();
+    //var food = food.GetRandomFood();
+    res.end(JSON.stringify(test));
 
-    res.end(JSON.stringify(queryAsObject));
+}).listen(3000);
 
-});
+console.log("Server listening on port 3000");
