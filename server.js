2b4bce4f37a235aca0bf53f06572a09b012c187e
DanX <dan@dan-solutions.dk>
2014-03-18 08:57:38 +0100

    port change

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/',function(req, res){
     res.send('Hello');
 });
 
-var server =  app.listen(3000, function(){
+var server =  app.listen(process.env.PORT || 3000, function(){
     console.log('Listening on port 3000');
 });
 
